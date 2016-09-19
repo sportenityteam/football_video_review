0f872ec80c0325fd2e542a3742e9302f4a31251d
krupalimakwana <krupali@complitech.net>
2016-09-19 16:00:25 +0530

    Profile for different users

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -46,7 +46,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :gender, :date_of_birth, :position, :zipcode,:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :gender, :date_of_birth, :position, :zipcode,:avatar,:current_club,:address,:description,:phone_number])
   end
 
   # The path used after update profile
