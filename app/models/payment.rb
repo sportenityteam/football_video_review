9aacfb6c93bc47bd5af93d08d07f6750abc5eadb
Mittal Patel <mittal@complitech.net>
2016-09-22 17:42:53 +0530

    Add changes in payment method

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,7 +5,7 @@ class Payment < ActiveRecord::Base
   # Validating the card automatically detects the card type
   def self.validate_credit_card(first_name,last_name,number,month,year,verification_value)
   	if first_name.present? and last_name.present?
-	  	@credit_card ||= ActiveMerchant::Billing::CreditCard.new(
+	  	@credit_card = ActiveMerchant::Billing::CreditCard.new(
 	      :first_name         => first_name,
 	      :last_name          => last_name,
 	      :number             => number,
@@ -14,7 +14,7 @@ class Payment < ActiveRecord::Base
 	      :verification_value => verification_value,
 	    )
 	  else
-	  	@credit_card ||= ActiveMerchant::Billing::CreditCard.new(
+	  	@credit_card = ActiveMerchant::Billing::CreditCard.new(
 	      :first_name         => first_name,
 	      :last_name          => first_name,
 	      :number             => number,
