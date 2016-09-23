888f4d43faccfae5cc91b0ac6f8eb7b098c58f2d
Mittal Patel <mittal@complitech.net>
2016-09-23 16:05:14 +0530

    Admin can add and destroy age group

--- app/controllers/admin/prices_controller.rb
+++ app/controllers/admin/prices_controller.rb
@@ -1,6 +1,7 @@
 class Admin::PricesController < Admin::BaseController
 
   #respond_to :html, :json
+  before_action :set_price, :only => [:show,:destroy]
 
   def index
     @prices = Price.all
@@ -15,18 +16,22 @@ class Admin::PricesController < Admin::BaseController
 
   def create
     @price = Price.new(price_params)
+    respond_to do |format|
+      if @price.save
+        format.html { redirect_to admin_prices_path, :notice => 'Price was successfully added.' }
+        format.json { render :show, status: :created, location: @price }
+      else
+        format.html { render :new }
+        format.json { render json: @price.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
   end
 
   def update
-    # @price = Price.find(params[:id])
-    # @price.update_attributes(price_params)
-    # respond_with @price
-
     @price = Price.find params[:id]
-
     respond_to do |format|
       if @price.update_attributes(price_params)
         format.html { redirect_to admin_prices_path, :notice => 'Price was successfully updated.' }
@@ -38,9 +43,21 @@ class Admin::PricesController < Admin::BaseController
     end
   end
 
+  def destroy
+    @price.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_prices_path, notice: 'Age group was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
     def price_params
       params.require(:price).permit!#(:name, :price)
+    end
+
+    def set_price
+      @price = Price.find(params[:id])
     end
 end
