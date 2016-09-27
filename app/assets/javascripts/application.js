f6d85a634aad414382f15fb8223ab254ae958a6d
Mittal Patel <mittal@complitech.net>
2016-09-27 14:50:36 +0530

    remove best in place from js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery_nested_form
-//= require best_in_place
+// require best_in_place
 //= require bootstrap
 //= require SpryTabbedPanels
 //= require custom
