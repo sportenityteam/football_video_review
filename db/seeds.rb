92d73cf8cb2754bc56ed3a4fec5872c7134df861
arth-complitech <arth@complitech.net>
2016-10-03 14:05:53 +0530

    seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(email: "admin@example.com", password: "12345678", first_name: "Admin", last_name: "Admin", date_of_birth: "1990-12-02", gender: 1, position: "", zipcode: "380061", user_type: 1, address: nil, phone_number: nil, current_club: nil, description: nil, soccer_club: nil, soccer_goal: nil)
+
+Price.create!(name: "12 and under", price: 150)
+Price.create!(name: "13 and up", price: 200)
