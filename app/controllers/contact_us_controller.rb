784826f35b07b2b2d71b1e88efe6ccf218e16b9f
Mittal Patel <mittal@complitech.net>
2016-10-07 11:11:24 +0530

    Add reply to in contact us email

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -3,18 +3,25 @@ class ContactUsController < ApplicationController
 
   def new
     @contact_us = ContactU.new
-  end 
+  end
 
   def contact_us_mail
     @contact_us = ContactU.new(contact_us_params)
-    @contact_us.save
-    @admin_user = User.where(:user_type => 1)
-    @admin_user.each do |admin|
-      ContactUsMailer.send_inquiry_email(@contact_us,admin).deliver_now!
-    end
-    respond_to do |format|
-      format.html { redirect_to contact_us_mail_path, notice: 'Message send successfully.' }
-      format.json { head :no_content }
+    if params[:contact_u][:email].present?
+      @contact_us.save
+      @admin_user = User.where(:user_type => 1)
+      @admin_user.each do |admin|
+        ContactUsMailer.send_inquiry_email(@contact_us,admin).deliver_now!
+      end
+      respond_to do |format|
+        format.html { redirect_to contact_us_mail_path, notice: 'Message send successfully.' }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to contact_us_mail_path, notice: 'Please enter your email address.' }
+        format.json { head :no_content }
+      end
     end
   end
 
--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -5,6 +5,6 @@ class ContactUsMailer< ActionMailer::Base
   	@message_body = contact_us.description
     @email = contact_us.email
     @subject = contact_us.subject.present? ? contact_us.subject : "Football Video Review: Inquiry"
-    mail(:to => admin.email , :subject => @subject,:from => contact_us.email)
+    mail(:to => admin.email , :subject => @subject,:reply_to => contact_us.email)
   end
 end
