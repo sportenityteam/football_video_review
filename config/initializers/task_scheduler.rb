c309902f6bdd944eefbfb1b177fd240764df3474
Mittal Patel <mittal@complitech.net>
2016-12-08 17:32:21 +0530

    Add changes in scheduler

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -6,13 +6,18 @@ scheduler = Rufus::Scheduler.new
 
 if Rails.env.downcase == "production"
   #Run rake jobs for ppaperclip style and transcoder
-  scheduler.every "12h" do
+  scheduler.every "1m" do
     Rails.logger.warn "---------------------Production Mode-------------"
-    `rake jobs:workoff RAILS_ENV=production`
-    `rake jobs:work RAILS_ENV=production`
+    `bin/delayed_job stop RAILS_ENV=production`
+    `bin/delayed_job start RAILS_ENV=production`
   end
 
 else
+  scheduler.every "1m" do
+    Rails.logger.warn "---------------------Development Mode-------------"
+    `bin/delayed_job stop`
+    `bin/delayed_job start`
+  end
   Rails.logger.warn "---------------------Development Mode-------------"
 end
 
