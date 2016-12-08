ef78b9e2beec68183e4978fcbe2f9b93e08848b7
Mittal Patel <mittal@complitech.net>
2016-12-08 17:43:48 +0530

    Add changes for delayed job

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -9,14 +9,14 @@ if Rails.env.downcase == "production"
   scheduler.every "1m" do
     Rails.logger.warn "---------------------Production Mode-------------"
     `bin/delayed_job stop RAILS_ENV=production`
-    `bin/delayed_job start RAILS_ENV=production`
+    `bin/delayed_job start --exit-on-complete RAILS_ENV=production`
   end
 
 else
   scheduler.every "1m" do
     Rails.logger.warn "---------------------Development Mode-------------"
     `bin/delayed_job stop`
-    `bin/delayed_job start`
+    `bin/delayed_job start --exit-on-complete`
   end
   Rails.logger.warn "---------------------Development Mode-------------"
 end
