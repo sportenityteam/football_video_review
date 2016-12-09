2e73b7f1a40c71bce489810f8c3e197c2abaf55b
Mittal Patel <mittal@complitech.net>
2016-12-09 15:07:25 +0530

    Stop auto running delayed job

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -6,18 +6,17 @@ scheduler = Rufus::Scheduler.new
 
 if Rails.env.downcase == "production"
   #Run rake jobs for ppaperclip style and transcoder
-  scheduler.every "1m" do
-    Rails.logger.warn "---------------------Production Mode-------------"
-    `bin/delayed_job stop RAILS_ENV=production`
-    `bin/delayed_job start --exit-on-complete RAILS_ENV=production`
-  end
-
+  # scheduler.every "1m" do
+  #   Rails.logger.warn "---------------------Production Mode-------------"
+  #   `bin/delayed_job stop RAILS_ENV=production`
+  #   `bin/delayed_job start --exit-on-complete RAILS_ENV=production`
+  # end
 else
-  scheduler.every "1m" do
-    Rails.logger.warn "---------------------Development Mode-------------"
-    `bin/delayed_job stop`
-    `bin/delayed_job start --exit-on-complete`
-  end
+  # scheduler.every "1m" do
+  #   Rails.logger.warn "---------------------Development Mode-------------"
+  #   `bin/delayed_job stop`
+  #   `bin/delayed_job start --exit-on-complete`
+  # end
   Rails.logger.warn "---------------------Development Mode-------------"
 end
 
