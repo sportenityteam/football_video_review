c67d2783883a31039937862e77b2defcd09eba8a
Mittal Patel <mittal@complitech.net>
2016-12-12 15:13:39 +0530

    Remove video thumb1 style

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,21 +3,15 @@ class Video < ActiveRecord::Base
   belongs_to :order
 
   #paperclip
-  # has_attached_file :video_url,
-  #                   :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-  #                   :url => "/system/:attachment/:id/:style/:filename",
-  #                   :styles => {
-  #                               :medium => { :geometry => "800x400<", :format => 'mp4' },
-  #                               :thumb1 => { :geometry => "120x120#", :format => 'jpg', :time => 1 },
-  #                               :thumb => { :geometry => "512x512#", :format => 'jpg', :time => 1 }
-  #                              }, :processors => [:transcoder]
+  has_attached_file :video_url,
+                    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+                    :url => "/system/:attachment/:id/:style/:filename",
+                    :styles => {
+                                :medium => { :geometry => "800x400<", :format => 'mp4' },
+                                :thumb => { :geometry => "512x512#", :format => 'jpg', :time => 5 }
+                               }, :processors => [:transcoder]
 
   #process_in_background :video_url
-
-  has_attached_file :video_url,
-                  :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-                  :url => "/system/:attachment/:id/:style/:filename",
-                  :styles => { :medium => "800x400<", :thumb1 => "120x120#", :thumb => "512x512#" }
 
   #validations
   validates_attachment_content_type :video_url, :content_type => /\Avideo\/.*\Z/
