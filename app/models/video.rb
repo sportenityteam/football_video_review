e32aa71d3998c5bcb3d7a69d233971d31c46a9bd
Mittal Patel <mittal@complitech.net>
2016-12-22 10:36:51 +0530

    Uncomment paperclip auto transcoder

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,7 +7,7 @@ class Video < ActiveRecord::Base
                     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
                     :url => "/system/:attachment/:id/:style/:filename",
                     :styles => {
-                                # :medium => { :geometry => "800x400<", :format => 'mp4' },
+                                :medium => { :geometry => "800x400<", :format => 'mp4' },
                                 :thumb1 => { :geometry => "120x120#", :format => 'jpg', :time => 1 },
                                 :thumb => { :geometry => "512x512#", :format => 'jpg', :time => 1 }
                                }, :processors => [:transcoder]
