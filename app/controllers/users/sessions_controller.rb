75055cf2adbe62b137f1ba2eab53ec7c4cc46349
Mittal Patel <mittal@complitech.net>
2016-12-29 16:51:39 +0530

    Add changes for destroy user

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
+  skip_before_filter :require_no_authentication, only: [:destroy]
 
   # GET /resource/sign_in
   # def new
@@ -13,11 +14,11 @@ class Users::SessionsController < Devise::SessionsController
 
   # DELETE /resource/sign_out
   # clear all session variables while user logout
-  # def destroy
-  #   reset_session
-  #   sign_out current_user
-  #   super
-  # end
+  def destroy
+    reset_session
+    sign_out current_user
+    super
+  end
 
   # protected
 
