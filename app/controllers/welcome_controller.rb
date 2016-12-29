029535fedf7503b0505c6bdcdd86c03551257be4
Mittal Patel <mittal@complitech.net>
2016-12-29 18:00:37 +0530

    Add authenticate user in welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
 	before_filter :restrict_user
   def index
     if current_user.present?
