fd88438f451b59f578864a90ac6c91b8d67e66d9
Mittal Patel <mittal@complitech.net>
2016-12-29 18:18:42 +0530

    Revert changes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  #before_action :configure_sign_in_params, only: [:create]
-  #skip_before_filter :require_no_authentication, only: [:destroy]
-
-  prepend_before_filter :force_reset_session, only: :destroy
+# before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -16,14 +13,10 @@ class Users::SessionsController < Devise::SessionsController
 
   # DELETE /resource/sign_out
   # clear all session variables while user logout
-  # def destroy
-  #   reset_session
-  #   sign_out current_user
-  #   super
-  # end
-
-  def force_reset_session
+  def destroy
     reset_session
+    sign_out current_user
+    super
   end
 
   # protected
