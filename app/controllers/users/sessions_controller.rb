8fd0b6ce60d4364f74af6e25ea980e437324929a
Mittal Patel <mittal@complitech.net>
2016-12-29 16:40:14 +0530

    Comment session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,9 +2,9 @@ class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
   # def create
@@ -13,11 +13,11 @@ class Users::SessionsController < Devise::SessionsController
 
   # DELETE /resource/sign_out
   # clear all session variables while user logout
-  def destroy
-    reset_session
-    sign_out current_user
-    super
-  end
+  # def destroy
+  #   reset_session
+  #   sign_out current_user
+  #   super
+  # end
 
   # protected
 
